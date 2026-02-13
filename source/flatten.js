fded8964ab514726a9b07a21440c9d7902ec93fb
klumaks <maxklubkov@gmail.com>
2026-02-13 13:54:15 +0300

    flatten web22 Клубков Максим

---
+++ source/flatten.js
@@ -0,0 +1,32 @@
+'use strict';
+
+/**
+ * Рекурсивно "разворачивает" многомерный массив в одномерный
+ * @param {Array} array - Входной массив с произвольным уровнем вложенности
+ *
+ * @example
+ * // возвращает [1, 2, 3]
+ * flatten([1, [2, [3]]]);
+ *
+ * @example
+ * // возвращает []
+ * flatten([]);
+ *
+ * @returns {Array} Новый одномерный массив, содержащий все элементы исходного массива
+ */
+
+const flatten = (array) => {
+    let result = [];
+
+    for (let i = 0; i < array.length; i++) {
+        const item = array[i];
+
+        if (Array.isArray(item)) {
+            result = result.concat(flatten(item));
+        } else {
+            result.push(item);
+        }
+    }
+
+    return result;
+};
